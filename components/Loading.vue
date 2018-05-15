b47ca0a83dc8dbcb523402f984eec0185e169c2e
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-05-15 16:09:22 +0300

    Add loading

---
+++ components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <transition name="el-loading-fade" @after-leave="handleAfterLeave">
+    <div v-show="visible" class="el-loading-mask">
+      <div class="el-loading-spinner">
+        <svg class="circular" viewBox="25 25 50 50">
+          <circle class="path" cx="50" cy="50" r="20" fill="none"/>
+        </svg>
+        <p v-if="text" class="el-loading-text">{{ text }}</p>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: null,
+    visible: false
+  }),
+
+  methods: {
+    handleAfterLeave () {
+      this.$emit('after-leave')
+    }
+  }
+}
+</script>

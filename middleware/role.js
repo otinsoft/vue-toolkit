0a6b3b00ec770f4dab05dcd654816f9d5477521d
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-05-29 16:39:50 +0300

    Check for user

--- middleware/role.js
+++ middleware/role.js
@@ -2,7 +2,7 @@ export default (to = '/') => ({ store, route, redirect }) => {
   const user = store.getters['auth/user']
   const [role] = route.meta.map(meta => meta.role).filter(role => role)
 
-  if (role && user.role !== role) {
+  if (user && role && user.role !== role) {
     return redirect(to)
   }
 }

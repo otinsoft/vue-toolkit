e9871e512f7d11ca65dd154d24fc8ff608150e0b
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-07-25 17:16:12 +0300

    Add checkbox group

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,16 +1,46 @@
 <template>
-  <label class="custom-control custom-checkbox mb-0">
+  <label
+    :class="[
+      { 'is-disabled': isDisabled },
+      { 'is-checked': isChecked }
+    ]"
+    :aria-checked="indeterminate ? 'mixed': isChecked"
+    :aria-disabled="isDisabled"
+    :id="id"
+    class="custom-control custom-checkbox"
+    role="checkbox"
+  >
     <input
-      :id="id"
+      v-if="trueLabel || falseLabel"
       :name="name"
-      type="checkbox"
-      :checked="internalValue"
+      :disabled="isDisabled"
+      :true-value="trueLabel"
+      :false-value="falseLabel"
+      v-model="model"
       class="custom-control-input"
-      v-indeterminate="indeterminate"
-      @click="handleClick">
-    <span class="custom-control-label font-weight-normal">
+      type="checkbox"
+      aria-hidden="true"
+      @change="handleChange"
+      @focus="focus = true"
+      @blur="focus = false">
+    <input
+      v-else
+      :disabled="isDisabled"
+      :value="label"
+      :name="name"
+      v-model="model"
+      class="custom-control-input"
+      type="checkbox"
+      aria-hidden="true"
+      @change="handleChange"
+      @focus="focus = true"
+      @blur="focus = false">
+
+    <span v-if="$slots.default || label" class="custom-control-label">
       <slot/>
+      <template v-if="!$slots.default">{{ label }}</template>
     </span>
+
     <slot name="feedback"/>
   </label>
 </template>
@@ -20,46 +50,121 @@ export default {
   name: 'Checkbox',
 
   props: {
-    id: { type: String, default: null },
-    name: { type: String, default: 'checkbox' },
-    value: { type: Boolean, default: false },
+    value: { type: [String, Number], default: undefined },
+    label: { type: String, default: undefined },
+    indeterminate: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
     checked: { type: Boolean, default: false },
-    indeterminate: { type: Boolean, default: false }
+    name: { type: String, default: undefined },
+    trueLabel: { type: [String, Number], default: undefined },
+    falseLabel: { type: [String, Number], default: undefined },
+    id: { type: String, default: undefined },
+    controls: { type: String, default: undefined }
   },
 
-  data () {
-    return { internalValue: this.value }
-  },
+  data: () => ({
+    selfModel: false,
+    focus: false,
+    isLimitExceeded: false
+  }),
 
-  watch: {
-    value (val) {
-      this.internalValue = val
-    },
+  computed: {
+    model: {
+      get () {
+        return this.isGroup
+          ? this.store : this.value !== undefined
+            ? this.value : this.selfModel
+      },
 
-    checked (val) {
-      this.internalValue = val
-    },
+      set (val) {
+        if (this.isGroup) {
+          this.isLimitExceeded = false;
+          (this._checkboxGroup.min !== undefined &&
+            val.length < this._checkboxGroup.min &&
+            (this.isLimitExceeded = true));
 
-    internalValue (val, oldVal) {
-      if (val !== oldVal) {
-        this.$emit('input', val)
+          (this._checkboxGroup.max !== undefined &&
+            val.length > this._checkboxGroup.max &&
+            (this.isLimitExceeded = true))
+
+          this.isLimitExceeded === false &&
+          this._checkboxGroup.$emit('input', val)
+        } else {
+          this.$emit('input', val)
+          this.selfModel = val
+        }
       }
+    },
+
+    isChecked () {
+      if ({}.toString.call(this.model) === '[object Boolean]') {
+        return this.model
+      } else if (Array.isArray(this.model)) {
+        return this.model.indexOf(this.label) > -1
+      } else if (this.model !== null && this.model !== undefined) {
+        return this.model === this.trueLabel
+      }
+    },
+
+    isGroup () {
+      let parent = this.$parent
+      while (parent) {
+        if (parent.$options.componentName !== 'CheckboxGroup') {
+          parent = parent.$parent
+        } else {
+          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+          this._checkboxGroup = parent
+          return true
+        }
+      }
+      return false
+    },
+
+    store () {
+      return this._checkboxGroup ? this._checkboxGroup.value : this.value
+    },
+
+    isDisabled () {
+      return this.isGroup
+        ? this._checkboxGroup.disabled || this.disabled
+        : this.disabled
     }
   },
 
   created () {
-    if ('checked' in this.$options.propsData) {
-      this.internalValue = this.checked
+    this.checked && this.addToStore()
+  },
+  mounted () {
+    if (this.indeterminate) {
+      this.$el.setAttribute('aria-controls', this.controls)
     }
   },
 
   methods: {
-    handleClick (e) {
-      this.$emit('click', e)
-
-      if (!e.isPropagationStopped) {
-        this.internalValue = e.target.checked
+    addToStore () {
+      if (
+        Array.isArray(this.model) &&
+        this.model.indexOf(this.label) === -1
+      ) {
+        this.model.push(this.label)
+      } else {
+        this.model = this.trueLabel || true
       }
+    },
+    handleChange (ev) {
+      if (this.isLimitExceeded) return
+      let value
+      if (ev.target.checked) {
+        value = this.trueLabel === undefined ? true : this.trueLabel
+      } else {
+        value = this.falseLabel === undefined ? false : this.falseLabel
+      }
+      this.$emit('change', value, ev)
+      this.$nextTick(() => {
+        if (this.isGroup) {
+          this._checkboxGroup.$emit('change', this._checkboxGroup.value)
+        }
+      })
     }
   }
 }
---
+++ components/CheckboxGroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="custom-checkbox-group" role="group" aria-label="checkbox-group">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxGroup',
+
+  componentName: 'CheckboxGroup',
+
+  props: {
+    value: { type: Array, default: undefined },
+    disabled: { type: Boolean, default: false },
+    min: { type: Number, default: undefined },
+    max: { type: Number, default: undefined }
+  }
+}
+</script>
